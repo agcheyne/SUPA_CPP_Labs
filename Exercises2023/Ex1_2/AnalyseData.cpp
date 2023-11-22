ccd291a5831eec315df2692dcb1cd4744345fe4f
agcheyne <147097354+agcheyne@users.noreply.github.com>
2023-11-22 20:58:06 +0000

    Moved functions to Custom functions
    Made terminal output more reader friendly
    
    Changed "another function" choice to outside switch instead of inside cases

--- Exercises2023/Ex1_2/AnalyseData.cpp
+++ Exercises2023/Ex1_2/AnalyseData.cpp
@@ -14,11 +14,7 @@
 #include <boost/tuple/tuple.hpp>
 #include <algorithm>
 
-
 #include "CustomFunctions.h"
-//#include "gnuplot-iostream.h"
-
-
 //syntax to run
 //g++ CustomSource.cpp Main.cpp -o MyProgram
 //to copy and paste beacause I'm lazy - g++ CustomFunctions.cpp AnalyseData.cpp -o MyProgram
@@ -62,89 +58,8 @@ y.push_back(std::stod(yin[i]));
 
 }
 
-
-std::tuple<double, double, double> fitLine(std::pair<std::vector<double>, std::vector<double>> xy, std::pair<std::vector<double>, std::vector<double>> xy_err)
-{
-    int N = xy.first.size();
-    double p, q, chi;
-    
-    double sum_xi = 0;
-    double sum_yi = 0; 
-    double sum_xisq = 0;
-    double sum_xiyi = 0;
-    
-  for(int i = 0; i < N; i++) 
-    {
-        sum_xi += xy.first[i];
-        sum_yi += xy.second[i];
-        sum_xisq += xy.first[i]*xy.first[i];
-        sum_xiyi += xy.first[i]*xy.second[i];
-    }
-
-    p = ((N*sum_xiyi)-(sum_xi*sum_yi)) / ((N*sum_xisq)-(sum_xi*sum_xi));
-    q = ((sum_xisq*sum_yi)-(sum_xiyi*sum_xi)) / ((N*sum_xisq)-(sum_xi*sum_xi));
-
-    // debuggin steps - left for reference
-    // std::cout << "Sum of xi: " << sum_xi << std::endl;
-    // std::cout << "Sum of yi: " << sum_yi << std::endl;
-    // std::cout << "Sum of xiyi: " << sum_xiyi << std::endl;
-    // std::cout << "Sum of xisquared: " << sum_xisq << std::endl;
-    
-    std::cout << "For this set of x,y data. The straight line fit (y = px + q) have the parameters: " << std::endl;
-    std::cout << "p = " << p << std::endl;
-    std::cout << "q = " << q << std::endl;
-
-    //used to work, now broken
-    // expect - p = 0.126266, q = 0.405255
-    // chi squared = 311.694    (sum of chi squared)    
-    // N = 25   (number of data points)         
-    // nParameters = 2 (number of parameters in fit)    
-    // NDF = N - nParameters = 23 (degrees of freedom)
-    // Goodness of fit = chi squared / NDF = 311.694/23 = 13.5519
-   
-
-
-
-    //calculating chi squared
-    double chiSum;
-    for(int i = 0; i < N; i++) 
-    {
-        double expected = (xy.first[i] * p) + q;    //expected y value for given x - based on fit
-        double diff = xy.second[i] - expected;      //difference between expected and actual y value
-        double numerator = diff * diff;
-        double denominator = (p * p * xy_err.first[i] * xy_err.first[i]) + (xy_err.second[i] * xy_err.second[i]);   //error on y value - (from data in file and error from fit)
-        chiSum += numerator / denominator;          //sum of chi squared for each point
-
-    // for(int i = 0; i < N; i++) 
-    // {
-    //    chiSum = chiSum + ( (xy.second[i] - ((xy.first[i]*p) + q))*(xy.second[i] - ((xy.first[i]*p) + q)) ) / ( sqrt( (p*p*xy_err.first[i]*xy_err.first[i]) + (xy_err.second[i]*xy_err.second[i])  ));
-    // }
-
-    }
-
-    int nParameters = 2;
-    int NDF = N - nParameters;
-    double GoodofFit = chiSum/(NDF); //goodness of fit - chi squared divided by degrees of freedom
-
-    std::cout << "Sum of chisq: " << chiSum << std::endl;
-    std::cout << "As we have "<< N <<" data points, and " << nParameters << " parameters to our fit. Thus, we have " << N-nParameters << " Degrees of freedom." << std::endl;
-    std::cout << "ChiSquared/NDF = " << chiSum << "/" << NDF << " = " << GoodofFit << std::endl;
-
-    std::ofstream myfile; //output file stream
-    myfile.open ("fitParams.txt"); //open file to write to 
-    myfile << "p = " << p << std::endl;
-    myfile << "q = " << q << std::endl;
-    myfile << "ChiSquared/NDF = " << chiSum << "/" << N-2 << " = " << GoodofFit << std::endl;
-    myfile.close(); //close file
-
-
-    return std::make_tuple(p,q,GoodofFit);
-
-}
-
 int main()
 {
-
 std::string filePath =  "/workspaces/SUPA_CPP_Labs/Exercises2023/Ex1_2/input2D_float.txt";
 std::string filePath_err = "/workspaces/SUPA_CPP_Labs/Exercises2023/Ex1_2/error2D_float.txt";
 
@@ -158,10 +73,11 @@ while (go){
 
     int n_lines = xy.first.size();
     std::printf("\nYou have read in a file with %i lines of data, corresponding to %i x,y coordinates. \n", n_lines, n_lines);
-    std::printf("To print some lines of the data, enter 1. \n");
-    std::printf("To calculate the magnitude of each point, press 2. \n");
-    std::printf("To output fit parameters for this data, enter 3. \n");
-    std::printf("To calculate x^y for this data, enter 4. \n");
+    std::printf("Choose from the following options:\n");
+    std::printf("1. Prints a chosen number of lines of the data file. \n");
+    std::printf("2. Calculates the magnitude of each data point. \n");
+    std::printf("3. Fits the data to a straight line. \n");
+    std::printf("4. Calculates x^y for the data. \n");
     std::printf("To exit, enter 0. \n");
     std::cout << std::endl;
 
@@ -170,124 +86,46 @@ while (go){
     switch (i) {
 
         case 0: 
-            std::cout << "Exiting." << std::endl;
+            std::cout << "Exiting program..." << std::endl;
             go = false;
             break;
+
         case 1: 
             std::cout << std::endl;
             lineprint(xy);
-            std::cout << std::endl;
-
-            std::cout << "If you woud like perform another function, press 1." << std::endl;     
-            std::cout << "Otherwise, press 0 to exit." << std::endl;
-           
-            std::cin >> j;
-              switch (j)
-                {
-                case 1:
-                    std::cout << std::endl;
-                    break;
-                
-                case 0: 
-                    std::cout << "Exiting." << std::endl;
-                     go = false;
-                    break;
-
-                default:
-                    std::cout << "invalid choice. Exiting." << std::endl;
-                    go = false;
-                    break;
-                }
             break;
 
         case 2: 
             std::cout << std::endl;
-            vectorCalc(xy);
-            std::cout << std::endl;
-            
-            std::cout << "If you woud like perform another function, press 1." << std::endl;     
-            std::cout << "Otherwise, press 0 to exit." << std::endl;
-           
-            std::cin >> j;
-              switch (j)
-                {
-                case 1:
-                    std::cout << std::endl;
-                    break;
-                
-                case 0: 
-                    std::cout << "Exiting." << std::endl;
-                     go = false;
-                    break;
-
-                default:
-                    std::cout << "invalid choice. Exiting." << std::endl;
-                    go = false;
-                    break;
-                }
+            vectorCalc(xy);        
             break;
+
         case 3: 
             std::cout << std::endl;
             fitLine(xy, xy_err);
-            std::cout << std::endl;
-            
-            std::cout << "If you woud like perform another function, press 1." << std::endl;     
-            std::cout << "Otherwise, press 0 to exit." << std::endl;
-           
-            std::cin >> j;
-              switch (j)
-                {
-                case 1:
-                    std::cout << std::endl;
-                    break;
-                
-                case 0: 
-                    std::cout << "Exiting." << std::endl;
-                     go = false;
-                    break;
-
-                default:
-                    std::cout << "invalid choice. Exiting." << std::endl;
-                    go = false;
-                    break;
-                }
             break;
+
         case 4:   
             std::cout << std::endl;
             XsupYCalc(xy);
-            std::cout << std::endl;
-            
-            std::cout << "If you woud like perform another function, press 1." << std::endl;     
-            std::cout << "Otherwise, press 0 to exit." << std::endl;
-           
-            std::cin >> j;
-              switch (j)
-                {
-                case 1:
-                    std::cout << std::endl;
-                    break;
-                
-                case 0: 
-                    std::cout << "Exiting." << std::endl;
-                     go = false;
-                    break;
-
-                default:
-                    std::cout << "invalid choice. Exiting." << std::endl;
-                    go = false;
-                    break;
-                }
             break;
         
-        default:{
-            std::cout << "invalid choice. Exiting." << std::endl;
-            go = false;
+        default:
+            std::cout << "invalid choice. Exiting..." << std::endl;
             break;
-        }
-
-
     }
+
+    if (go) {
+        std::cout << std::endl;
+        std::cout << "Would you like to perform another function? (1 for Yes, 0 for No)" << std::endl;
+        std::cin >> j;
+        if (j != 1) {
+            std::cout << "Exiting program...\n";
+            go = false;
+        }
+    }  
 }
+
 
 
 return 0;
